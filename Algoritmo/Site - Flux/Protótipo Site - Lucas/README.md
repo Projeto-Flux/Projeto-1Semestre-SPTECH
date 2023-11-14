e83fb6d0dc8533c01e8943f74d9c5b523effa4d4
Enzo Martins <enzo.martins@sptech.school>
2023-11-14 18:23:47 -0300

    Alterando Api

--- Algoritmo/Site - Flux/Protótipo Site - Lucas/README.md
+++
@@ -1,2 +0,0 @@
-# site-institucional-Flux_2.0
-Site institucional dedicado ao projeto Flux. Versão 2.0
--- api/site/src/models/usuarioModel.js
+++ api/site/src/models/usuarioModel.js
@@ -11,12 +11,12 @@ function autenticar(email, senha) {
 
 // Coloque os mesmos parâmetros aqui. Vá para a var instrucao
 function cadastrar(nomeServer, emailServer, cnpjServer, senhaServer, cepServer,numeroServer) {
-    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nomeServer, emailServer, cpfServer, senhaServer);
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nomeServer, emailServer, senhaServer);
     
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
     var instrucao = `
-        INSERT INTO usuario (nome,  cnpj, senha, cep, numero) VALUES ('${nomeServer}', '${emailServer}', '${cnpjServer}', '${senhaServer}',${cepServer},${numeroServer});
+        INSERT INTO shopping (nome,email,  cnpj, senha, cep, numero) VALUES ('${nomeServer}', '${emailServer}', '${cnpjServer}', '${senhaServer}',${cepServer},${numeroServer});
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
