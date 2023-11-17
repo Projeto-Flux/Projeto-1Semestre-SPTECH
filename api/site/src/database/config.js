207f5082af2739db60564a3fa8531f77b9a38189
Enzo Martins <enzo.martins@sptech.school>
2023-11-17 15:23:17 -0300

    Api Node

--- api/site/src/database/config.js
+++ api/site/src/database/config.js
@@ -22,7 +22,7 @@ var mySqlConfig = {
     host: "localhost",
     database: "flux",
     user: "root",
-    password: "#Gf51603570845",
+    password: "Pereira2004",
 };
 
 function executar(instrucao) {
